95a49279280de4e44d5675038776693ddc641a3b
Denis Koronchik <denis.koronchik@gmail.com>
2017-03-22 01:17:49 +0300

    [scs] Implement diagnostic

--- src/scs/scsServer.ts
+++ src/scs/scsServer.ts
@@ -6,11 +6,14 @@ import {
 	TextDocuments, TextDocument, Diagnostic, DiagnosticSeverity, 
 	InitializeParams, InitializeResult,
     TextDocumentPositionParams, CompletionItem,
-    Hover
+    Hover, DidChangeTextDocumentParams,
+    WorkspaceSymbolParams, SymbolInformation
 } from 'vscode-languageserver';
 
 import { SCsHoverProvider } from './scsHovers';
 import { SCsCompletionItemProvider } from './scsCompletion';
+import { SCsParsedData } from './scsParsedData';
+import { getFilesInDirectory, getFileContent } from './scsUtils';
 
 // Create a connection for the server. The connection uses Node's IPC as a transport
 let connection: IConnection = createConnection(new IPCMessageReader(process), new IPCMessageWriter(process));
@@ -22,24 +25,45 @@ let documents: TextDocuments = new TextDocuments();
 // for open, change and close text document events
 documents.listen(connection);
 
-documents.onDidChangeContent((event) => {
-    connection.console.log("Content changed");
-});
-
 // After the server has started the client sends an initialize request. The server receives
 // in the passed params the rootPath of the workspace plus the client capabilities. 
 class CoreContext {
     hoverProvider: SCsHoverProvider;
     completionProvider: SCsCompletionItemProvider;
 
+    parsedData: SCsParsedData;
+
     constructor() {
         this.hoverProvider = new SCsHoverProvider();
         this.completionProvider = new SCsCompletionItemProvider();
+
+        this.parsedData = new SCsParsedData();
+        this.parsedData.sendDiagnostic = connection.sendDiagnostics;
     }
 };
 
 let coreCtx: CoreContext;
 let workspaceRoot: string;
+
+function parseAllOpenedDocuments() {
+    documents.all().forEach((doc: TextDocument, index: number, array: TextDocument[]) => {
+        connection.console.log("Parse " + doc.uri);
+        coreCtx.parsedData.parseDocument(doc.getText(), doc.uri);
+    });
+}
+
+function parseDocumentsInFolder(path: string) {
+    connection.console.log("Parse files in: " + path);
+    
+    let files: string[] = getFilesInDirectory(path, ['scs', 'scsi']);
+    files.forEach((filePath: string) => {
+        const content: string = getFileContent(filePath).toString();
+        coreCtx.parsedData.parseDocument(content, filePath);
+
+        connection.console.log(filePath + ": " + coreCtx.parsedData.errors[filePath].length);
+    });
+    
+}
 
 connection.onInitialize((params): InitializeResult => {
 	
@@ -51,22 +75,31 @@ connection.onInitialize((params): InitializeResult => {
 			// Tell the client that the server works in FULL text document sync mode
 			textDocumentSync: documents.syncKind,
             hoverProvider: true,
-            completionProvider: { resolveProvider: true }
+            completionProvider: { resolveProvider: true },
+            workspaceSymbolProvider: true 
             }
 		}
 });
 
 connection.onDidChangeConfiguration((params) => {
     connection.console.log("Configuration changed");
+
+    parseAllOpenedDocuments();
 });
 
 connection.onDidChangeWatchedFiles(() => {
     connection.console.log("Watched files changed");
+
+    parseAllOpenedDocuments();
 });
 
 connection.onHover((params: TextDocumentPositionParams) : Hover => {
     let doc: TextDocument = documents.get(params.textDocument.uri);
     return coreCtx.hoverProvider.provide(doc, params.position);
+});
+
+connection.onWorkspaceSymbol((params: WorkspaceSymbolParams) : SymbolInformation[] => {
+    return null;
 });
 
 connection.onCompletion((params: TextDocumentPositionParams) : CompletionItem[] => {
@@ -76,6 +109,11 @@ connection.onCompletion((params: TextDocumentPositionParams) : CompletionItem[] 
 
 connection.onCompletionResolve((item: CompletionItem) : CompletionItem => {
     return coreCtx.completionProvider.resolve(item);
+});
+
+documents.onDidChangeContent((event) => {
+    connection.console.log("Content changed: " + event.document.uri);
+    coreCtx.parsedData.parseDocument(event.document.getText(), event.document.uri);
 });
 
 // Listen on the connection
---
+++ src/scs/syntax/scsSyntax.pegjs
@@ -0,0 +1,111 @@
+{
+    // common functions
+}
+
+syntax 
+    = (_ sentence_comment _)*
+
+sentence_comment
+ 	= COMMENT
+    / (sentence ';;')
+
+sentence
+    = sentence_lvl1 { return "sentence_lvl1"; }
+	/ sentence_lvl_common { return "sentence_lvl_common"; }
+    
+idtf_lvl1_preffix
+	= 'sc_node'
+    / 'sc_link'
+    / 'sc_edge_dcommon'
+    / 'sc_edge_ucommon'
+    / 'sc_edge_main'
+    / 'sc_edge_access'
+    
+idtf_lvl1_value
+ 	= (idtf_lvl1_preffix '#')? idtf_system
+    
+idtf_lvl1
+	= idtf_lvl1_value
+    / LINK
+
+idtf_system
+	= ID_SYSTEM
+    / '...' { return "unnamed"; }
+
+idtf_edge "edge"
+	= '(' idtf_system
+	      connector attr_list?
+	      idtf_system
+	  ')'
+	
+idtf_set "set"
+	= '{' attr_list? idtf_common (';' attr_list? idtf_common)* '}'
+
+idtf_common
+	= idtf_system
+	/ idtf_edge
+	/ idtf_set
+	/ CONTENT
+	/ LINK
+
+idtf_list
+	= _ idtf_common _ internal_sentence_list? (_ ';' _ idtf_common _ internal_sentence_list?)*
+
+internal_sentence "internal sentence"
+	= _ connector _ attr_list? idtf_list
+
+internal_sentence_list
+	= '(*' _ (internal_sentence ';;')+ _ '*)'
+
+sentence_lvl1
+ 	= idtf_lvl1 _ '|' _ idtf_lvl1 _ '|' _ idtf_lvl1
+
+sentence_lvl_common
+    = idtf_common _ connector _ attr_list? _ idtf_list (';' _ connector _ attr_list? idtf_list)*
+
+attr_list "attributes list"
+    = (ID_SYSTEM _ EDGE_ATTR)+
+
+connector "connector"
+    = ('<>' /
+      '..>' / '<..' /
+      '<=>' / '=>' / '<=' /
+      '->' / '<-' / 
+      '-|>' / '<|-' /
+      '-/>' / '</-' /
+      '~>'  / '<~'  /
+      '~|>' / '<|~' /
+      '~/>' / '</~' /
+       '_<>' /
+      '_..>'/ '_<..'/
+      '_<=>'/ '_=>' / '_<=' /
+      '_->' / '_<-' /
+      '_-|>'/ '_<|-'/
+      '_-/>'/ '_</-'/
+      '_~>' / '_<~' /
+      '_~|>'/ '_<|~'/
+      '_~/>'/ '_</~'/
+       '>' / '<' /
+       '_>' / '_<'  )
+
+// ------------------------------------------------
+ID_SYSTEM "system identifier"
+    = _ ([.]+)? ([_]?) [a-zA-Z0-9_]+ _
+
+EDGE_ATTR "edge attribute modifier"
+    = (
+        ':' / '::'
+      )
+
+COMMENT "comments"
+    = ('//' ([^\n])*) { return "comment"; }
+    / ("/*" (!"*/" .)* "*/")
+
+LINK "link to file"
+    = '"' (!'"' .)* '"'
+
+CONTENT "content"
+    = '[' [^\]]* ']'
+
+_ "whitespace"
+    = [ \t\r\n]*
