185149435f121f4b8cd9390beaef468312be1396
Denis Koronchik <denis.koronchik@gmail.com>
2017-08-17 21:14:14 +0300

    Work on new syntax analyzer (antlr)

--- src/scs/scsParsedData.ts
+++ src/scs/scsParsedData.ts
@@ -2,9 +2,37 @@
 
 import * as vs from 'vscode-languageserver';
 
+import { ANTLRInputStream, CommonTokenStream, ParserErrorListener } from 'antlr4ts';
+
 import { makeUri } from './scsUtils';
 
-const scsParser = require('./scsSyntax');
+const scsParser = require('./syntax/scsParser');
+const scsLexer = require('./syntax/scsLexer');
+
+interface ParseError {
+    line: number,
+    offset: number,
+    len: number,
+    msg: string
+}
+
+class ErrorListener implements ParserErrorListener {
+
+    private callback:(err: ParseError) => void = null;
+
+    constructor(callback) {
+        this.callback = callback;
+    }
+
+    syntaxError(recognizer, offendingSymbol, line, charPositionInLine, msg: string, e) : void {
+        this.callback({
+            line: line,
+            offset: charPositionInLine,
+            len: offendingSymbol ? offendingSymbol.text.length : 0,
+            msg: msg
+        });
+    }
+};
 
 interface SymbolPosition
 {
@@ -29,12 +57,12 @@ function isSymbolRangeEqual(a: SymbolRange, b:SymbolRange) : boolean {
 function getSymbolRange(location) : SymbolRange {
     return {
         start: {
-            line: location.start.line,
-            column: location.start.column
+            line: location.line,
+            column: location.offset
         },
         end: {
-            line: location.end.line,
-            column: location.end.column
+            line: location.line,
+            column: location.offset + location.len
         }
     };
 }
@@ -48,7 +76,7 @@ function toRange(range: SymbolRange): vs.Range {
 class FileInfo
 {
     private uri: string;             // uri of a file
-    private errors: vs.Diagnostic[];    // list of all errors is a file
+    public errors: vs.Diagnostic[];    // list of all errors is a file
     private symbols: Map<string, SymbolRange[]>;       // map of all symbol occurenses
 
     constructor(docUri: string) {
@@ -57,10 +85,9 @@ class FileInfo
         this.symbols = new Map<string, SymbolRange[]>();
     }
 
-    public appendError(err) : void {
-        let range = vs.Range.create(err.location.start.line - 1, err.location.start.column - 1,
-                                 err.location.end.line - 1, err.location.end.column - 1);
-        let diagnostic = vs.Diagnostic.create(range, err.toString());
+    public appendError(err: ParseError) : void {
+        let range = vs.Range.create(err.line, err.offset, err.line, err.offset + err.len);
+        let diagnostic = vs.Diagnostic.create(range, err.msg);
 
         this.errors.push(diagnostic);
     }
@@ -133,21 +160,28 @@ export class SCsParsedData
     public parseDocument(docText: string, docUri: string) {
         
         docUri = makeUri(docUri);
-        let finfo = new FileInfo(docUri);
+        const finfo = new FileInfo(docUri);
         this.files.set(docUri, finfo);
 
         try {
-            scsParser.parse(docText, {
-                docUri: docUri,
-                parsedData: this
-            });
+
+            let chars = new ANTLRInputStream(docText);
+            let lexer = new scsLexer.scsLexer(chars);
+            let tokens  = new CommonTokenStream(lexer);
+            let parser = new scsParser.scsParser(tokens);
+            parser.buildParseTrees = false;
+
+            parser.docUri = docUri;
+            parser.parsedData = this;
+
+            parser.addErrorListener(new ErrorListener(function(err) {
+                finfo.appendError(err);
+            }));
+
+            let tree = parser.syntax();
+
         } catch (e) {
-            if (e instanceof scsParser.SyntaxError)
-            {
-                finfo.appendError(e);
-            } else {
-                this.console.log(e.stack);
-            }
+            this.console.log(e.stack);
         }
 
         // send diagnostic
@@ -229,16 +263,30 @@ export class SCsParsedData
         return result;
     }
 
-    public _onAppendSymbol(docUri: string, name:string, location) : void {
-        // correct location
-
-        name = name.trim();
+    public _onAppendSymbol(docUri: string, name: string, location) : void {
         const finfo = this.files.get(docUri);
 
         if (!finfo)
             return; // we need to work safe
 
+        name = name.trim();
         // append symbol
         finfo.appendSymbol(name, getSymbolRange(location));
+    }
+
+    public _onAppendError(docUri: string, err: ParseError) : void {
+        const finfo = this.files.get(docUri);
+
+        this.console.log(docUri, err.msg);
+
+        if (!finfo)
+            return; // we need to work safe
+
+        // append symbol
+        finfo.appendError(err);
+    }
+
+    public _log(msg: string) {
+        this.console.log(msg);
     }
 };
